efd0c1adb9417053ca5a5f1b1668af17d21b8569
ItZRockyboi <112095621+ItZRockyboi@users.noreply.github.com>
2023-03-13 17:51:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup(){
+video = createCapture(VIDEO);
+video.size(555,555);
+canvas = createCanvas(650,700);
+canvas.position(750,75)
+}
+
+
+function draw(){
+
+}
+
+
+function preload(){
+
+}
